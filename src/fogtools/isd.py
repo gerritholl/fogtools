85b35647d73a34e0bd9286f218f0f51b5bd6bfdc
Gerrit Holl <gerrit.holl@dwd.de>
2020-01-21 16:00:20 +0100

    More functionality related to ISD
    Added more functionality related to ISD, along with complete unit test
    coverage.

--- src/fogtools/isd.py
+++ src/fogtools/isd.py
@@ -2,12 +2,197 @@
 
 """
 
+import os
+import pathlib
+import numpy
 import pandas
-
-url_station_list = "ftp://ftp.ncdc.noaa.gov/pub/data/noaa/isd-history.txt"
+import pkg_resources
 
 def get_stations():
-    """Download the list of ISD stations
+    """Return a list of ISD stations as a pandas DataFrame
+
+    Returns:
+
+        pandas.DataFrame with the stations
     """
-    return pandas.read_fwf(url_station_list, skiprows=20,
-                           parse_dates=["BEGIN", "END"])
+    station_list = pkg_resources.resource_filename(
+                    "fogtools", "data/isd-history.txt")
+    df = pandas.read_fwf(station_list, skiprows=20,
+                         parse_dates=["BEGIN", "END"],
+                         dtype={"WBAN": "O", "USAF": "O"})
+    # The first line of the dataframe is blank.  Normally, one should be able
+    # to pass skip_blank_lines=True to read_fwf, but this has no effect:
+    #
+    # https://github.com/pandas-dev/pandas/issues/22693
+    #
+    # therefore, explicitly drop the first entry
+
+    return df.drop(0)
+
+def select_stations(df,
+        states=["RI", "MA", "VT", "NH", "ME", "CT", "NY"]):
+    """From the stations list, select those we want
+
+    From the stations list returned by :func:`get_stations`, return only those
+    that are in New England or New York and active in 2020.
+
+    Args:
+        df (pandas.DataFrame)
+            DataFrame with a list of stations, such as returned by
+            :func:`get_stations`.
+        states (List, optional)
+            List of US states to consider.  Defaults to New England states +
+            NY.
+
+    Returns:
+        pandas.DataFrame with only those stations we want to consider
+    """
+    return df[df.isin({"ST": states}).any(1) & (df.END>"20200101")]
+
+def get_station_ids(df):
+    """Get station IDs from station list
+
+    From the station list (DataFrame), obtain a pandas.Series of IDs for
+    direct download through AWS noaa-global-hourly-pds, which consists of
+    the columns USAF and WBAN.
+
+    Args:
+        df (pandas.DataFrame):
+            DataFrame with station list, such as from :func:`get_stations`
+
+    Returns:
+        pandas.Series with IDs.
+    """
+    return df["USAF"] + df["WBAN"]
+
+def _obj_to_str(df):
+    """Ensure all object-dtypes in df contain only str
+
+    When a pandas DataFrame has a column with strings, it will make an object
+    dtype.  However, if it contains a mixture of strings and ints, this object
+    dtype may have poor performance as an HDF.  See
+    https://stackoverflow.com/q/22998859/974555 and answers there.
+
+    Args:
+        df (pandas.DataFrame):
+            Dataframe on which to operate.
+
+    Returns nothing, operates in-place.
+    """
+
+    oc = [k for (k, v) in df.dtypes.items() if v==numpy.dtype("O")]
+    df.loc[:, oc] = df[oc].applymap(str)
+
+def dl_station(year, id_):
+    """Download station from AWS.
+
+    Download the measurements for a station and year from AWS S3 and return it
+    as a pandas DataFrame.
+
+    Args:
+        year (int):
+            Year to download
+        id_ (str):
+            Station ID as returned by :func:`get_station_ids`
+
+    Returns:
+        pandas.DataFrame with station contents.
+    """
+
+    df = pandas.read_csv(
+         f"s3://noaa-global-hourly-pds/{year:04d}/{id_:s}.csv",
+         dtype={"STATION": str})
+    _obj_to_str(df)
+    return df
+
+def _get_cache_dir(base=None):
+    """Get directory to use for caching
+
+    Get (and create, if necessary) directory to use for caching.
+
+    Args:
+        base (str or pathlib.Path):
+            Directory in which to create cache dir.  If not given, use
+            XDG_CACHE_HOME or otherwise ~/.cache.
+
+    Returns:
+        pathlib.Path object pointing to cache dir
+    """
+    cacheroot = (base or
+                 os.environ.get("XDG_CACHE_HOME") or
+                 pathlib.Path.home() / ".cache")
+    cacheroot = pathlib.Path(cacheroot)
+    cacheroot /= "fogtools"
+    cacheroot.mkdir(parents=True, exist_ok=True)
+    return cacheroot
+
+def get_station(year, id_):
+    """Get station as DataFrame from cache or AWS.
+
+    Try to get a station from local disk cache ($XDG_CACHE_HOME/fogtools if
+    this is set, otherwise ~/.cache/fogtools).  If it's not there, get it from
+    AWS, then store it to cache.
+
+    Args:
+        year (int):
+            Year to download
+        id_ (str):
+            Station ID as returned by :func:`get_station_ids`
+
+    Returns:
+        pandas.DataFrame with station contents.
+    """
+
+    cachedir = _get_cache_dir()
+    cachefile = (cachedir / str(year) / id_).with_suffix(".h5")
+    try:
+        return pandas.read_hdf(cachefile, "root")
+    except FileNotFoundError:
+        df = dl_station(year, id_)
+        df.to_hdf(cachefile, "root")
+        return df
+
+def extract_vis(df):
+    """From a measurement dataframe, extract visibilities
+
+    Visibilities are reported in the AWS ISD CSV files with a sort of nested
+    CSV.  This function unpacks the string and reports visibility, visibility
+    quality code, visibility variability code, and visibility quality
+    variability code.  See the ISD format document, page 10.
+
+    Args:
+        df (pandas.DataFrame):
+            DataFrame with station list, such as from :func:`get_stations`
+
+    Returns:
+        pandas.DataFrame with four visibilities named vis, vis_qc, vis_vc, and
+        vis_qvc.
+    """
+    tmp = df.VIS.str.extract(r"(\d{6}),(\d),([NV9]),(\d)")
+    tmp.columns = ["vis", "vis_qc", "vis_vc", "vis_qvc"]
+    vis = pandas.to_numeric(
+            tmp["vis"],
+            errors="coerce").fillna(999999).astype(numpy.int32)
+    tmp.drop("vis", inplace=True, axis=1)
+    tmp["vis"] = vis
+    return tmp
+
+def create_db(f=None):
+    """Create a HDF5 database with all New England measurements
+
+    Args:
+        f (str or pathlib.Path)
+            Where to write the database.  Defaults to a file "store.h5" in
+            the cache directory.  File will be overwritten.
+    """
+    stations = select_stations(get_stations())
+    ids = get_station_ids(stations)
+    cachedir = _get_cache_dir()
+    f = f or (cachedir / "store.h5")
+    with pandas.HDFStore(f, mode="w", complevel=3) as store:
+        for (id_, st, fi) in zip(ids, stations["BEGIN"], stations["END"]):
+            for year in pandas.date_range(
+                    pandas.Timestamp(st.year, 1, 1),
+                    pandas.Timestamp(fi.year+1, 1, 1), freq="Y").year:
+                df = get_station(year, id_)
+                store.append("root", df)
---
+++ tests/test_isd.py
@@ -0,0 +1,128 @@
+import os
+import io
+import tempfile
+
+import pytest
+import numpy
+import pandas
+import pkg_resources
+from unittest import mock
+
+csv_test_content=""""STATION","DATE","SOURCE","LATITUDE","LONGITUDE","ELEVATION","NAME","REPORT_TYPE","CALL_SIGN","QUALITY_CONTROL","WND","CIG","VIS","TMP","DEW","SLP","AA1","AA2","AY1","AY2","GF1","MW1","REM"
+"94733099999","2019-01-03T22:00:00","4","-32.5833333","151.1666666","45.0","SINGLETON STP, AS","FM-12","99999","V020","050,1,N,0010,1","22000,1,9,N","025000,1,9,9","+0260,1","+0210,1","99999,9","24,0000,9,1",,"0,1,02,1","0,1,02,1","01,99,1,99,9,99,9,99999,9,99,9,99,9","01,1","SYN05294733 11/75 10502 10260 20210 60004 70100 333 70000="
+"94733099999","2019-01-04T04:00:00","4","-32.5833333","151.1666666","45.0","SINGLETON STP, AS","FM-12","99999","V020","090,1,N,0021,1","22000,1,9,N","025000,1,9,9","+0378,1","+0172,1","99999,9","06,0000,9,1",,"0,1,02,1","0,1,02,1","03,99,1,99,9,99,9,99999,9,99,9,99,9","03,1","SYN04294733 11/75 30904 10378 20172 60001 70300="
+"94733099999","2019-01-04T22:00:00","4","-32.5833333","151.1666666","45.0","SINGLETON STP, AS","FM-12","99999","V020","290,1,N,0057,1","99999,9,9,N","020000,1,9,9","+0339,1","+0201,1","99999,9","24,0000,9,1",,"0,1,02,1","0,1,02,1",,"02,1","SYN05294733 11970 02911 10339 20201 60004 70200 333 70000="
+"94733099999","2019-01-05T22:00:00","4","-32.5833333","151.1666666","45.0","SINGLETON STP, AS","FM-12","99999","V020","200,1,N,0026,1","99999,9,9,N","000100,1,9,9","+0209,1","+0193,1","99999,9","24,0004,3,1",,"1,1,02,1","1,1,02,1","08,99,1,99,9,99,9,99999,9,99,9,99,9","51,1","SYN05294733 11/01 82005 10209 20193 69944 75111 333 70004="
+"94733099999","2019-01-08T04:00:00","4","-32.5833333","151.1666666","45.0","SINGLETON STP, AS","FM-12","99999","V020","070,1,N,0026,1","22000,1,9,N","025000,1,9,9","+0344,1","+0213,1","99999,9","06,0000,9,1",,"2,1,02,1","2,1,02,1","04,99,1,99,9,99,9,99999,9,99,9,99,9","02,1","SYN04294733 11/75 40705 10344 20213 60001 70222="
+"""
+
+@pytest.fixture
+def stations():
+    from fogtools.isd import get_stations
+    return get_stations()
+
+@pytest.fixture
+def subset(stations):
+    from fogtools.isd import select_stations
+    return select_stations(stations)
+
+@pytest.fixture
+def station():
+    from fogtools.isd import dl_station
+    with mock.patch("s3fs.S3FileSystem", autospec=True) as s3:
+        s3.return_value.open.return_value = io.StringIO(csv_test_content)
+        return dl_station(2019, "94733099999")
+
+def test_get_stations(stations):
+    # get_stations gets called in fixture
+    assert len(stations)==29742
+    assert stations["END"].max() == pandas.Timestamp("2020-01-15")
+    assert stations["BEGIN"].min() == pandas.Timestamp("1901-01-01")
+    assert stations["WBAN"].dtype == stations["USAF"].dtype == numpy.dtype("O")
+    assert (stations["LAT"].dtype == stations["LON"].dtype ==
+            stations["ELEV(M)"].dtype == numpy.dtype("f8"))
+    assert stations["BEGIN"].dtype == stations["END"].dtype == numpy.dtype("<M8[ns]")
+
+def test_select_stations(subset):
+    # select_stations get called in fixture
+    assert len(subset) == 167
+    assert subset["END"].min() > pandas.Timestamp("2020-01-01")
+    assert (subset["CTRY"]=="US").all()
+
+def test_get_station_ids(subset):
+    from fogtools.isd import get_station_ids
+    ids = get_station_ids(subset)
+    assert len(ids) == len(subset)
+    assert isinstance(ids, pandas.Series)
+    assert (ids.str.len()==11).all()
+
+def test_dl_station(station):
+    # dl_station gets called in fixture
+    assert len(station)==5
+    assert (station.STATION == "94733099999").all()
+
+def test_extract_vis(station):
+    from fogtools.isd import extract_vis
+    df_vis = extract_vis(station)
+    assert df_vis["vis"].min() == 100
+    assert df_vis["vis"].max() == 25000
+    assert (df_vis["vis_qc"]=="1").all()
+    assert (df_vis["vis_vc"]=="9").all()
+    assert (df_vis["vis_qvc"]=="9").all()
+
+def test_cache_dir():
+    from fogtools.isd import _get_cache_dir
+    with tempfile.TemporaryDirectory() as tmpdir:
+        d = _get_cache_dir(tmpdir)
+        assert d.exists()
+        assert d.is_dir()
+        assert str(d.parent) == tmpdir
+        assert d.name == "fogtools"
+    try:
+        d = _get_cache_dir()
+        assert d.exists()
+        assert d.is_dir()
+        assert d.parent.name == ".cache"
+        assert d.name == "fogtools"
+    finally:
+        d.rmdir()
+    try:
+        _environ = os.environ.copy()
+        os.environ["XDG_CACHE_HOME"] = os.environ.get("TMPDIR", "/tmp")
+        d = _get_cache_dir()
+        assert d.exists()
+        assert d.is_dir()
+        assert str(d.parent) == "/tmp"
+        assert d.name == "fogtools"
+    finally:
+        d.rmdir()
+        os.environ.clear()
+        os.environ.update(_environ)
+
+def test_get_station():
+    from fogtools.isd import get_station
+    # need to mock/test case in which file supposedly exists
+    # and case where it doesn't
+    with mock.patch("pandas.read_hdf", autospec=True) as pr:
+        get_station(2020, "1234567890")
+        pr.assert_called_once()
+
+    with mock.patch("pandas.read_hdf", side_effect=FileNotFoundError,
+                    autospec=True) as pr, \
+            mock.patch("fogtools.isd.dl_station", autospec=True) as ds:
+        get_station(2020, "1234567890")
+        ds.assert_called_once_with(2020, "1234567890")
+        ds.return_value.to_hdf.assert_called_once()
+
+@mock.patch("fogtools.isd.select_stations", autospec=True)
+@mock.patch("pandas.HDFStore", autospec=True)
+@mock.patch("pandas.read_csv", autospec=True)
+def test_create_db(pr, ph, ss, stations):
+    from fogtools.isd import create_db
+    ss.return_value = stations.iloc[18000:18005]
+    create_db()
+    ss.assert_called_once()
+    ph.assert_called_once()
+    n = 166 # station-years in those 5 cases
+    assert pr.call_count==n
+    assert ph.return_value.__enter__.return_value.append.call_count==n
