b413813029c9e7a5370693f5670f9321edb90ee2
Gerrit Holl <gerrit.holl@dwd.de>
2020-02-06 17:39:35 +0100

    Added script to show severe fog days

---
+++ tests/test_fogdays.py
@@ -0,0 +1,26 @@
+"""Test the fogfreq script
+"""
+
+from unittest.mock import patch
+import pytest
+
+
+@patch("argparse.ArgumentParser", autospec=True)
+def test_get_parser(ap):
+    import fogtools.analysis.fogrank
+    fogtools.analysis.fogrank.get_parser()
+    assert ap.return_value.add_argument.call_count == 3
+
+
+@patch("fogtools.analysis.fogrank.get_parser")
+def test_main(gp):
+    import fogtools.analysis.fogrank
+    gp.return_value.parse_args.return_value.n = 5
+    gp.return_value.parse_args.return_value.v = 1000
+    gp.return_value.parse_args.return_value.f = "csv"
+    fogtools.analysis.fogrank.main()
+    gp.return_value.parse_args.return_value.f = "markdown"
+    fogtools.analysis.fogrank.main()
+    gp.return_value.parse_args.return_value.f = "invalid"
+    with pytest.raises(ValueError):
+        fogtools.analysis.fogrank.main()
