0ec39c40bc02eb231738740971735f57690f64d1
Gerrit Holl <gerrit.holl@dwd.de>
2020-02-11 14:17:01 +0100

    Add test for get_dl_dest
    Added test function for get_dl_dest

--- tests/test_abi.py
+++ tests/test_abi.py
@@ -28,6 +28,17 @@ def test_s3_select(sS):
             "s3://noaa-goes16/ABI-L1b-RadC/2020/061/12/*C10*")
 
 
+def test_get_dl_dest():
+    from fogtools.abi import get_dl_dest
+
+    b = pathlib.Path("/tmp")
+    t = pandas.Timestamp("2020-03-01T12")
+    assert get_dl_dest(b, t, 42, "lettuce") == \
+        pathlib.Path("/tmp/abi/2020/03/01/12/00/42/lettuce")
+    assert get_dl_dest(b, t, 42, "a/b/lettuce.nuts") == \
+        pathlib.Path("/tmp/abi/2020/03/01/12/00/42/lettuce.nuts")
+
+
 @patch("s3fs.S3FileSystem", autospec=True)
 def test_download_abi(sS):
     from fogtools.abi import download_abi_day
