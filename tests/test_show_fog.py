c82980ec30017fb4246be9d98496a010fe0c9e1c
Gerrit Holl <gerrit.holl@dwd.de>
2020-04-08 12:16:53 +0200

    Add ability to store dependencies + some cleanup
    Add the ability to store the dependencies, i.e. all datasets used to
    generate the fog composite, as tif files.  Also do some cleanup of the
    vis module.

--- tests/test_show_fog.py
+++ tests/test_show_fog.py
@@ -8,41 +8,43 @@ from unittest.mock import patch, MagicMock
 def test_get_parser(ap):
     import fogtools.processing.show_fog
     fogtools.processing.show_fog.get_parser()
-    assert ap.return_value.add_argument.call_count == 6
+    assert ap.return_value.add_argument.call_count == 7
 
 
 @patch("fogtools.processing.show_fog.parse_cmdline", autospec=True)
 @patch("fogtools.vis.get_fog_blend_for_sat", autospec=True)
 @patch("fogpy.composites.Scene", autospec=True)
-def test_main(fcS, fvg, fpsp, tmpdir):
+def test_main(fcS, fvg, fpsp, tmp_path):
     import fogtools.processing.show_fog
     fpsp.return_value = fogtools.processing.show_fog.get_parser().parse_args(
             ["/no/out/file",
              "--sat", "/no/sat/files",
              "--nwcsaf", "/no/nwcsaf/files",
-             "-a", "fribbulux xax",
+             "-a", "fribbulus xax",
              "-m", "seviri_l1b_hrit"])
+    fvg.return_value = (MagicMock(), MagicMock())
     fogtools.processing.show_fog.main()
     fvg.assert_called_once_with(
             "seviri_l1b_hrit",
             ["/no/sat/files"],
             ["/no/nwcsaf/files"],
-            "fribbulux xax",
-            "overview",
-            return_extra=False)
-    fvg.return_value.save.assert_called_once_with("/no/out/file")
+            "fribbulus xax",
+            "overview")
+    fvg.return_value[0].save.assert_called_once_with("/no/out/file")
     fvg.reset_mock()
+    fvg.return_value[0].reset_mock()
     fpsp.return_value = fogtools.processing.show_fog.get_parser().parse_args(
-            ["/no/out/file",
+            [str(tmp_path),
              "--sat", "/no/sat/files",
              "--nwcsaf", "/no/nwcsaf/files",
-             "-a", "fribbulux xax",
+             "-a", "fribbulus xax",
              "-m", "seviri_l1b_hrit",
-             "-e", "shadowlands.nc"])
-    fvg.return_value = (MagicMock(), MagicMock())
+             "-i"])
     fogtools.processing.show_fog.main()
+    fvg.return_value[0].save.assert_called_once_with(
+            str(tmp_path / "fog_blend.tif"))
     fcS.return_value.save_datasets.assert_called_once_with(
             writer="cf",
             datasets=fvg.return_value[1].__getitem__
                         .return_value.data_vars.keys.return_value,
-            filename="shadowlands.nc")
+            filename=str(tmp_path / "intermediates.nc"))
